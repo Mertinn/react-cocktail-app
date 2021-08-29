545d1c955870ca868bd07f0d75df88369ea1f7ee
Mertinn <marcin.rajs@wp.pl>
2021-08-29 18:50:02 +0200

    Fixed cocktailitem

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,22 @@ import CocktailsGrid from "./Components/CocktailsGrid/CocktailsGrid";
 import axios from "axios";
 import CocktailItem from "./Components/CocktailItem/CocktailItem";
 
+export interface ICocktail {
+  cocktail: {
+    idDrink: string;
+    strDrink: string;
+    strInstructions: string;
+    strDrinkThumb: string;
+    strImageAttribution: string;
+  };
+}
+
+interface ICocktails {
+  drinks?: [];
+}
+
 function App() {
-  const [cocktails, setCocktails] = useState([]);
+  const [cocktails, setCocktails] = useState<ICocktails>({});
   useEffect(() => {
     (async () => {
       const response = await axios.get(
@@ -17,13 +31,16 @@ function App() {
       setCocktails(data);
     })();
   }, []);
+
   return (
     <div className="App">
       <PageContainer>
         <Sidebar />
         <CocktailsGrid>
-          {cocktails &&
-            cocktails.map((cocktail) => <CocktailItem cocktail={cocktail} />)}
+          {Object.keys(cocktails).length > 0 &&
+            cocktails.drinks!.map((cocktail: ICocktail["cocktail"]) => (
+              <CocktailItem cocktail={cocktail} />
+            ))}
         </CocktailsGrid>
       </PageContainer>
     </div>
--- src/Components/CocktailItem/CocktailItem.tsx
+++ src/Components/CocktailItem/CocktailItem.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import "./CocktailItem.styles.css";
+import { ICocktail as IProps } from "../../App";
 
-interface IProps {
-  cocktail: {};
-}
-
-const CocktailItem = () => {
-  return <div className={"cocktail"}></div>;
+const CocktailItem: React.FC<IProps> = ({ cocktail }) => {
+  return <div className={"cocktail"}>{cocktail.strDrink}</div>;
 };
 
 export default CocktailItem;
